3d29ed897179844ec9630ba100e37f9cfa61e3b4
Cristian González <crdgonzalezca@unal.edu.co>
2019-10-29 21:13:09 -0500

    Config firebase

--- app/UI/Chat/ChatComponent.js
+++ app/UI/Chat/ChatComponent.js
@@ -1,23 +1,36 @@
 import React, { Component } from "react";
 
+import firestore from '@react-native-firebase/firestore';
+
 import ChatScreen from "./ChatScreen"
 
-class ChatComponent extends Component{
-  constructor(props){
+class ChatComponent extends Component {
+  constructor(props) {
     super(props);
     this.state = {
 
     }
+    console.log(this.getChannels());
+    console.log("ASDASD");
   }
 
-  changeToBack(){
+  async getChannels() {
+    // Read the document for user 'Ada Lovelace':
+    const documentSnapshot = await firestore()
+      .collection('channels')
+      .get();
+
+    console.log(documentSnapshot);
+  }
+
+  changeToBack() {
     this.props.navigation.popToTop();
   }
 
-  render(){
-    return(
-      <ChatScreen 
-       changeToBack = {() => this.changeToBack()}
+  render() {
+    return (
+      <ChatScreen
+        changeToBack={() => this.changeToBack()}
       />
     )
   }
--- app/UI/Chat/ChatInitialComponent.js
+++ app/UI/Chat/ChatInitialComponent.js
@@ -1,23 +1,39 @@
 import React, { Component } from "react";
 
+import firestore from '@react-native-firebase/firestore';
+
+
 import ChatScreen from "./ChatInitialScreen"
 
-class ChatInitialComponent extends Component{
-  constructor(props){
+class ChatInitialComponent extends Component {
+  constructor(props) {
     super(props);
     this.state = {
 
     }
   }
+  
+  async getChannels() {
+    const sent = 'brandonavilan@gmail.com'
+    const received = 'andreavilan@gmail.com'
+    const documentSnapshot = await firestore()
+      .collection('channels')
+      .doc(sent)
+      .collection(received)
+      .get();
+    console.log(documentSnapshot.docs.forEach((doc, _) => {
+      console.log(doc.data())
+    }))
+  }
 
-  changeToBack(){
+  changeToBack() {
     this.props.navigation.popToTop();
   }
 
-  render(){
-    return(
-      <ChatScreen 
-       changeToBack = {() => this.changeToBack()}
+  render() {
+    return (
+      <ChatScreen
+        changeToBack={() => this.changeToBack()}
       />
     )
   }
